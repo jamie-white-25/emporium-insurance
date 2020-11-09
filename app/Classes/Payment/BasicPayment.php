62a2248ce628c05ee16e72c2d38cbe3fd0bbeb40
jamie <50985235+jamie-white-25@users.noreply.github.com>
2020-11-09 20:50:33 +0000

    update

--- app/Classes/Payment/BasicPayment.php
+++ app/Classes/Payment/BasicPayment.php
@@ -3,11 +3,16 @@
 namespace App\Classes\Payment;
 
 use App\Classes\Payment\PaymentInterface;
+use Carbon\Carbon;
 
 class BasicPayment implements PaymentInterface{
+    protected $basicPaymentDate = [];
 
     public function payment_date($dates){
-
+        foreach($dates as $date){
+            
+        }   
+        return $basicPaymentDate;
     }
     
 } 
--- app/Classes/Payment/Payment.php
+++ app/Classes/Payment/Payment.php
@@ -3,11 +3,11 @@
 namespace App\Classes\Payment;
 
 class Payment {
-    protected $payment_date;
+    protected $paymentDate;
 
     public function get_payment_date(Array $dates,PaymentInterface $payment)
     {
-        $payment_date = $payment->payment_date($dates);
-        return $payment_date;
+        $paymentDate = $payment->payment_date($dates);
+        return $paymentDate;
     }
 }
